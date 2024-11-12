056ce23a7783529637a1e059e66ebf475e319fdd
broGideon <turuncev70@gmail.com>
2024-11-12 11:18:39 +0300

    finish five practice

--- Pract5-Arduino/src/main.cpp
+++ Pract5-Arduino/src/main.cpp
@@ -2,18 +2,38 @@
 #include <Wire.h>
 
 #define input A0
-#define output 11
+#define output 9
+
+int newBrightness = 0;
+bool isLedOn = false;
+
+void playMelody() {
+    tone(output, 523, 500);
+    delay(500);
+    tone(output, 587, 500);
+    delay(500);
+    tone(output, 659, 500);
+    delay(500);
+    noTone(output);
+}
 
 void setup() {
-   Wire.begin();
-    //pinMode(input, INPUT);
+    Wire.begin();
+    pinMode(input, INPUT);
     Serial.begin(9600);
 }
 
 void loop() {
+    newBrightness = map(analogRead(input), 0, 1023, 255, 0);
     Wire.beginTransmission(8);
-    Wire.write("Hello, Slave!");
+    Wire.write(newBrightness);
     Wire.endTransmission();
-    Serial.println("AAA");
-    delay(2000);
+    delay(100);
+    Wire.requestFrom(8, 32);
+    if (Wire.available()) {
+        isLedOn = Wire.read();
+        if (isLedOn) {
+            playMelody();
+        }
+    }
 }
